2d1f91616a45c6b74f1f378dce88398a2a896559
mehmet soner korucu22 <mehmetsonerkorucu@hotmail.com>
2023-11-12 12:33:16 +0300

    problem 2 edited.

--- hw1/hw1_2.py
+++ hw1/hw1_2.py
@@ -1,6 +1,5 @@
 from datetime import datetime as dt
 
 current_datetime = dt.now()
-
 formatted_date = current_datetime.strftime("%d-%m-%Y %H:%M:%S")
-print(formatted_date)
+print("Output1:\nTwinkle, twinkle, little star,\n\tHow I wonder what you are!\n\t\tUp above the world so high,\n\t\tLike a diamond in the sky.\nTwinkle, twinkle, little star,\n\tHow I wonder what you are!\nOutput2:\nCurrent date and time :\n%s", formatted_date)
